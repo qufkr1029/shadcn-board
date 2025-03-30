62a2dddf1198dc634d2e1514b2ae5aa9fcfeb6dd
jbpark <qufkr1029@gmail.com>
2025-03-30 15:19:46 +0900

    1. popover, progress, datepicker 컴포넌트 설치 2. README 수정 3. onClick라우팅 함수 useRouter 사용 4. create 페이지 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import styles from "./page.module.scss"
 
 function Home() {
+    const router = useRouter();
+
     return (
         <div className={styles.container}>
             <div className={styles.container__onBoarding}>
@@ -11,7 +16,13 @@ function Home() {
                     <span>2. Add boards to page</span>
                 </div>
                 {/* 페이지 추가 버튼 */}
-                <Button variant="outline" className="w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500">Add New Page</Button>
+                <Button
+                    variant="outline"
+                    className="w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500"
+                    onClick={()=>router.push("/create")}
+                >
+                    Add New Page
+                </Button>
             </div>
 
         </div>
